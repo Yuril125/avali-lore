7cb3b1b58212ebf6c38bbfd1055e06f375aa8291
Yuril <yuril@yuril.net>
2026-08-31 22:38:27 -0400

    feat(tooling): Add script defining astronomical constants

---
+++ scripts/astro_calculations.js
@@ -0,0 +1,8 @@
+// All quantities are in SI units
+
+const M_SUN = 1.989e30
+const R_SUN = 6.9599e8
+const L_SUN = 3.846e26
+const G = 6.6743e-11
+const AU = 149597870700
+const SIGMA = 5.670374419e-8 // Stefan–Boltzmann constant
